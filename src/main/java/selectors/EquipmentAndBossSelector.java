49049413ac22aac2fad4e5ced1c25a2d82df00ec
DeafultCodeBotNumber578 <ryansmorgan9@gmail.com>
2023-03-03 10:52:13 -0600

    Adding defence lowering spec selector

--- src/main/java/selectors/EquipmentAndBossSelector.java
+++ src/main/java/selectors/EquipmentAndBossSelector.java
@@ -4,6 +4,7 @@ import equipment.MeleeArmor;
 import equipment.MeleeEquipmentLoadout;
 import equipment.RangedEquipmentLoadout;
 import lombok.NoArgsConstructor;
+import statsandmodifiers.DefenceLoweringSpecs;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,11 +13,33 @@ import java.util.List;
 @NoArgsConstructor
 public class EquipmentAndBossSelector {
 
+    /*
+        .headgear()
+        .cape()
+        .necklace()
+        .body()
+        .legs()
+        .shield()
+        .hands()
+        .feet()
+        .ring()
+
+        Classes: MeleeArmor, RangedArmor, MageArmor
+     */
+
     public List<MeleeEquipmentLoadout> defineMeleeEquipmentLoadouts() {
         List<MeleeEquipmentLoadout> meleeEquipmentLoadouts = new ArrayList<>();
 
         MeleeEquipmentLoadout meleeLoadout1 =  MeleeEquipmentLoadout.builder()
                 .headgear(MeleeArmor.SERPENTINE_HELM)
+                .cape(MeleeArmor.INFERNAL_CAPE)
+                .necklace(MeleeArmor.AMULET_OF_TORTURE)
+                .body(MeleeArmor.BANDOS_CHESTPLATE)
+                .legs(MeleeArmor.BANDOS_TASSETS)
+                .shield(MeleeArmor.AVERNIC_DEFENDER)
+                .hands(MeleeArmor.FEROCIOUS_GLOVES)
+                .feet(MeleeArmor.DRAGON_BOOTS)
+                .ring(MeleeArmor.BERSERKER_RING)
                 .build();
 
         return meleeEquipmentLoadouts;
@@ -30,11 +53,16 @@ public class EquipmentAndBossSelector {
         return rangedEquipmentLoadouts;
     }
 
+    public DefenceLoweringSpecs defineSpecWeaponHits() {
+        return DefenceLoweringSpecs.builder()
+                .bgsSpecDamage(50)
+                .numberOfDWHSpecs(2)
+        .build();
+    }
     //TODO definePrayers();
     //TODO defineStats();
     //TODO definePotions();
     //TODO buildBossList();
-    //TODO defineSpecWeapons();
     //TODO defineTOALevel();
 
 
---
+++ src/main/java/statsandmodifiers/DefenceLoweringSpecs.java
@@ -0,0 +1,11 @@
+package statsandmodifiers;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@AllArgsConstructor
+@Builder
+public class DefenceLoweringSpecs {
+    int bgsSpecDamage;
+    int numberOfDWHSpecs;
+}

cf559bd588d482198a39406baabc0f137350e3ad
DeafultCodeBotNumber578 <ryansmorgan9@gmail.com>
2023-03-12 08:01:45 -0500

    Refactoring files and adding new PlayerLoadout class

--- src/main/java/bosses/GWDBosses.java
+++ src/main/java/bosses/GWDBosses.java
@@ -1,4 +1,17 @@
 package bosses;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public enum GWDBosses {
+    KREEARA(255, 260, 200, 180, 180, 180, 200, 200);
+
+    private final int hp;
+    private final int baseDefence;
+    private final int baseMagicLevel;
+    private final int stabDefence;
+    private final int slashDefence;
+    private final int crushDefence;
+    private final int magicDefence;
+    private final int rangedDefence;
 }
